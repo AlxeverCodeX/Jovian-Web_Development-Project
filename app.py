ab887e4ca2a83728697f96e2d68b54435c286e9e
Emmanuel Albert <salberthp89@gmail.com>
2023-04-06 08:06:06 +0100

    Pushing

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+
+JOBS = [
+    {
+    'id':1,
+    'title': 'Data Analyst',
+    'location': 'Nigeria',
+    'salary': 'N 350,000'},
+    {
+    'id':2,
+    'title': 'Data Scientist',
+    'location': 'Nigeria',
+    'salary': 'N 500,000'},
+    {
+    'id':3,
+    'title': 'Front-End Developer',
+    'location': 'Remote',
+    },
+    {
+    'id':4,
+    'title': 'Back-End Developer',
+    'location': 'Canada',
+    'salary': '$ 250,000'}
+]
+
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run( host= '0.0.0.0', debug=True) 
